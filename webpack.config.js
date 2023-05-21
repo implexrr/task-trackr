f05fd278b563119de5c3bc48315a4df1f2745871
Chenhao Wang <implexrr@gmail.com>
2023-05-21 19:38:07 -0400

    Initial repo setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  performance: {
+    assetFilter: function(assetFilename) {
+      return !assetFilename.endsWith('.jpg');
+    },
+  },
+
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+ module: {
+   rules: [
+     {
+       test: /\.css$/i,
+       use: ['style-loader', 'css-loader'],
+     },
+     {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
+   ],
+ },
+};
